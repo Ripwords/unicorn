d0934a084adc2190182ce56e133a25e918fee2a1
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2021-10-17 22:31:06 +0800

    minor update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,14 @@
 <script lang="ts" setup>
-import { mainStore } from "../store/pinia";
 import { ref } from "vue";
+import { mainStore } from "../store/pinia";
 import { appWindow } from "@tauri-apps/api/window";
 import { register, unregisterAll } from "@tauri-apps/api/globalShortcut";
 
+// Required because the event listener is at router-view
 defineEmits(['changeTheme'])
 
 const store = mainStore()
-const theme = ref("dark")
 const visible = ref(false)
-const changeIcon = () => {
-  if (theme.value === "dark") {
-    theme.value = "light"
-  } else {
-    theme.value = "dark"
-  }
-}
 
 // register global shortcut
 unregisterAll()
@@ -40,13 +33,8 @@ appWindow.listen("tauri://blur", () => {
 <template>
   <n-h1 class="center" style="margin-top: 25px;">Hello {{ store.dec }}</n-h1>
   <div class="center">
-    <n-button @click="store.dec++">Increment</n-button>
     <n-button @click="store.dec--">Decrease</n-button>
+    <n-button @click="store.dec++">Increment</n-button>
   </div>
-  <div class="center">
-    <n-button @click="$emit('changeTheme'); changeIcon()">
-      <i-akar-icons:moon-fill v-if="theme === 'dark'"></i-akar-icons:moon-fill>
-      <i-akar-icons:sun-fill v-if="theme === 'light'"></i-akar-icons:sun-fill>
-    </n-button>
-  </div>
+  
 </template>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,6 +1,18 @@
 <script lang="ts" setup>
 import { useRouter } from "vue-router";
 import { appWindow } from "@tauri-apps/api/window";
+import { ref } from "vue";
+
+defineEmits(['changeTheme'])
+
+const theme = ref("dark")
+const changeIcon = () => {
+  if (theme.value === "dark") {
+    theme.value = "light"
+  } else {
+    theme.value = "dark"
+  }
+}
 
 const router = useRouter()
 const save = async () => {
@@ -11,7 +23,13 @@ const save = async () => {
 </script>
 
 <template>
-  <n-h1 class="center">Settings</n-h1>
+  <n-h1 class="center" style="margin-top: 25px;">Settings</n-h1>
+  <div class="center">
+    <n-button @click="$emit('changeTheme'); changeIcon()">
+      <i-akar-icons:moon-fill v-if="theme === 'dark'"></i-akar-icons:moon-fill>
+      <i-akar-icons:sun-fill v-if="theme === 'light'"></i-akar-icons:sun-fill>
+    </n-button>
+  </div>
   <div class="center">
     <n-button @click="save()">Save</n-button>
   </div>
