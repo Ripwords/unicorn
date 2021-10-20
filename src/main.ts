90a5ee97773b42835c19b23536f5a1053ed003c1
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2021-10-20 15:52:45 +0800

    added logo

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,5 @@ import { createApp } from 'vue'
 import { createPinia } from "pinia"
 import App from './App.vue'
 import router from './router/index'
-// @ts-ignore
-var AutoLaunch = require('auto-launch')
 
-var unicornAutoLauncher = new AutoLaunch({
-  name: "unicorn",
-})
-
-unicornAutoLauncher.enable()
 createApp(App).use(createPinia()).use(router).mount('#app')
